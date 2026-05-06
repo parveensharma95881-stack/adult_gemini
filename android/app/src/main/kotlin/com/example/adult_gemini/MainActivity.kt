97da82abb883772be878ca79a263c2ca29006021
parveensharma95881-stack <parveensharma95881@gmail.com>
2026-05-07 00:37:39 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/adult_gemini/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/adult_gemini/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.adult_gemini
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-    // यहाँ कुछ मत लिख, कतई खाली रहने दे
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
