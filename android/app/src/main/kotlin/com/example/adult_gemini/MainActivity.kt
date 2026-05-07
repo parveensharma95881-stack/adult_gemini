fab4a4438591625b5e288cf8a19efac911c64adb
parveensharma95881-stack <parveensharma95881@gmail.com>
2026-05-07 19:41:57 +0530

    Update and rename MainActivity.kt to MainActivity.kt

--- android/app/src/main/kotlin/com/example/adult_gemini/MainActivity.kt
+++
@@ -1,3 +0,0 @@
-package com.example.adult_gemini
-import io.flutter.embedding.android.FlutterActivity
-class MainActivity: FlutterActivity() { }
---
+++ android/app/src/main/kotlin/com/example/balaji/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.balaji // नया पैकेज नाम
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
