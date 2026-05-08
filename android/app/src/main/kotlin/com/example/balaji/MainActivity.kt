11a57eb5a3063ed40d02e7588e24a7b31e1b4c01
parveensharma95881-stack <parveensharma95881@gmail.com>
2026-05-08 06:23:21 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/balaji/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/balaji/MainActivity.kt
@@ -1,6 +1,7 @@
-package com.example.balaji // नया पैकेज नाम
+package com.example.balaji
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    // यहाँ कुछ और लिखने की ज़रूरत नहीं है, v2 में यह खाली रहता है
 }
